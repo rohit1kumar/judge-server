aea39131d46fbe41556f449c8448afe25aa2ae75
Rohit Kumar <kumar1rohit@outlook.com>
2023-06-03 21:32:44 +0530

    add better comments in user route

--- routes/user.js
+++ routes/user.js
@@ -6,21 +6,35 @@ import {
 	getLogginUser,
 	updateUser
 } from '../controllers/user.js'
+
 import {
 	registerValidation,
 	loginValidation,
 	validate
 } from '../middlewares/validator.js'
+
 import { isAuthenticated } from '../middlewares/auth.js'
+
 import { methodNotAllowed } from '../helpers/errorHandler.js'
 
 const router = express.Router()
 
+// User Profile Route
 router.get('/profile', isAuthenticated, getLogginUser)
+
+// User Registration Route
 router.post('/register', registerValidation(), validate, registerUser)
+
+// User Login Route
 router.post('/login', loginValidation(), validate, loginUser)
+
+// User Logout Route
 router.post('/logout', logoutUser)
+
+// Update User Profile Route
 router.patch('/update', isAuthenticated, updateUser)
+
+// Catch All for Invalid HTTP Methods
 router.all('*', methodNotAllowed)
 
 export default router
